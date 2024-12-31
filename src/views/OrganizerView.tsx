0233572f09302258688eef8a8755b97a44118e57
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2024-12-30 21:18:30 -0600

    Cleanup

--- src/views/OrganizerView.tsx
+++ src/views/OrganizerView.tsx
@@ -4,9 +4,10 @@ import { Page } from "../components/Page";
 const OrganizerView = (props: { isOpen: boolean }) => {
     return (
         <Page isOpen={props.isOpen}>
-            <div class="mx-auto min-w-[36rem] max-w-[48rem] p-6 mt-2"><div class="flex flex-row items-center justify-between mb-2">
-                <span class="font-bold text-2xl">Organizer</span>
-            </div>
+            <div class="mx-auto min-w-[36rem] max-w-[48rem] p-6 mt-2">
+                <div class="flex flex-row items-center justify-between mb-2">
+                    <span class="font-bold text-2xl">Organizer</span>
+                </div>
             </div>
         </Page>
     )
